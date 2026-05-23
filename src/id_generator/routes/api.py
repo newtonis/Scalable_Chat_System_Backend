8133d222b135079de03f9ea6c696decc51bdabe9
newtonis <ariel.nowik@gmail.com>
2026-05-23 03:29:16 +0000

    feat: udpated id generator connection session configuration

--- src/id_generator/routes/api.py
+++ src/id_generator/routes/api.py
@@ -1,32 +1,7 @@
 from flask import Blueprint, request, jsonify, current_app, g
 from id_generator_lib import PostgresIdGenerator, DatabaseConn
-import asyncio
-import logging
-
 
 api_bp = Blueprint("api", __name__)
-
-
-def get_pg_db():
-    # Si no hay una conexión abierta en esta petición, la creamos
-    if 'pg_db' not in g:
-        host = current_app.config.get('POSTGRES_HOST')
-        database_conn = DatabaseConn(host)
-        database_conn.try_init_database()
-        g.pg_db = database_conn
-
-    return g.pg_db
-
-
-# Aquí ocurre la magia del cierre automático
-@api_bp.teardown_request
-def close_pg_db(exception=None):
-    # Extraemos la conexión de 'g' si es que se llegó a abrir
-    database_conn = g.pop('pg_db', None)
-    
-    if database_conn is not None:
-        logging.info("Closing PG Database...")
-        database_conn.close_connection()
 
 
 @api_bp.route("/generate_id", methods=["POST"])
@@ -42,10 +17,16 @@ async def generate_id():
     
     group_name = data["group_name"]
 
-    database_conn = get_pg_db()
+    
+    host = current_app.config.get('POSTGRES_HOST')
+    database_conn = DatabaseConn(host)
+    database_conn.try_init_database()
+    
     postgres_id_generator = PostgresIdGenerator(database_conn)
     new_id = postgres_id_generator.generate_new_id(group_name)
 
+    database_conn.close_connection()
+    
     return jsonify({
         "generated_id": new_id
     }), 200
